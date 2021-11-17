b2241c0ddc147c44073df156e14b5ffefdb2d3d1
benjamin <benjamin@localhost.localdomain>
2021-11-16 19:24:19 -0500

    Add shopping cart icon

--- src/Header.jsx
+++ src/Header.jsx
@@ -13,27 +13,38 @@ import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
 import Container from '@mui/material/Container';
 import ImageList from './ImageList';
 import Footer from './Footer';
 import { Link } from 'react-router-dom';
 
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
 export default function Header() {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Typography
-          variant="h6"
-          noWrap
-          componnerat="div"
-          sx={{
-            flexGrow: 1,
-            textAlign: 'center',
-          }}
-        >
-          <Link to="/">Ramazon</Link>
-        </Typography>
-      </Toolbar>
+      <Container maxWidth="md">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            componnerat="div"
+            sx={{
+              flexGrow: 1,
+              // textAlign: 'center',
+            }}
+          >
+            <Link to="/">Ramazon</Link>
+          </Typography>
+
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton size="large" edge="end" color="inherit">
+              <ShoppingCartIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 }
